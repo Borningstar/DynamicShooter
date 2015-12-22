2b35abee08b2152416ba55edfe50118628af41b6
Ben Morningstar <ben.morningstar@gmail.com>
2015-12-22 18:33:33 +1000

    Shield -> Made Interface

---
+++ Assets/Scripts/Items/Shields/IShield.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Items.Shields
+{
+    using Reactors;
+
+    public interface IShield
+    {
+        float MaximumShield { get; }
+        float RechargeAmount { get; }
+        float RechageDelay { get; }
+        float RechargeRate { get; }
+        float RechargeCost { get; }
+        float CurrentShield { get; }
+
+        float DealDamage(float damage);
+        string ToString();
+        void ConnectReactor(Reactor reactor);
+    }
+}
---
+++ Assets/Scripts/Items/Shields/Shield.cs
@@ -0,0 +1,76 @@
+﻿namespace Assets.Scripts.Items.Shields
+{
+    using UnityEngine;
+    using Reactors;
+
+    public class Shield : Item, IShield
+    {
+        public float MaximumShield { get; protected set; }
+        public float RechargeAmount { get; protected set; }
+        public float RechageDelay { get; protected set; }
+        public float RechargeRate { get; protected set; }
+        public float RechargeCost { get; protected set; }
+        public float CurrentShield { get; protected set; }
+
+        private float nextCharge;
+        private Reactor reactor;
+
+        void Start()
+        {
+            CurrentShield = MaximumShield;
+        }
+
+        void Update()
+        {
+            if (CurrentShield < 0)
+            {
+                CurrentShield = 0;
+            }
+
+            if (reactor != null)
+            {
+                if (Time.time > nextCharge && CurrentShield < MaximumShield)
+                {
+                    nextCharge = Time.time + RechargeRate;
+                    if (reactor.Drain(RechargeCost))
+                    {
+                        CurrentShield += RechargeAmount;
+                        if (CurrentShield > MaximumShield)
+                        {
+                            CurrentShield = MaximumShield;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        //return left over damage if shield goes into damage, else 0
+        public float DealDamage(float damage)
+        {
+            CurrentShield -= damage;
+
+            float remaining = 0.0f;
+
+            if (CurrentShield < 0)
+            {
+                remaining = Mathf.Abs(CurrentShield);
+                CurrentShield = 0;
+            }
+
+            nextCharge = RechageDelay;
+
+            return remaining;
+        }
+
+        public override string ToString()
+        {
+            return CurrentShield.ToString("0.0");
+        }
+
+        public void ConnectReactor(Reactor reactor)
+        {
+            this.reactor = reactor;
+        }
+    }
+}
